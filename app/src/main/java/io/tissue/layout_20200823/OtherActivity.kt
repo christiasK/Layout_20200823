a74f1694706d32f9b28596f6f79f0b1dd0a49990
natoro <admin@natoro.com>
2020-08-23 15:45:22 +0900

    실제로 다른 화면으로 이동하였다.

--- app/src/main/java/io/tissue/layout_20200823/OtherActivity.kt
+++ app/src/main/java/io/tissue/layout_20200823/OtherActivity.kt
@@ -1,11 +1,18 @@
 package io.tissue.layout_20200823
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnMainActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
